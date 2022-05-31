eb12d5e84c504d9c343758506045053acfbd668b
Jason.Nemecek <jasonnemecek@yahoo.com>
2022-05-31 14:32:27 -0600

    SQL

--- SQL/Temp_surface_texture.sql
+++ SQL/Temp_surface_texture.sql
@@ -3,6 +3,8 @@
 DECLARE @area VARCHAR(20);
 DECLARE @area_type INT ;
 DECLARE @major INT;
+DECLARE @InRangeTop INT;
+DECLARE @InRangeBot INT;
 
 -- Soil Data Access
 /*~DeclareChar(@area,20)~  -- Used for Soil Data Access
@@ -13,27 +15,51 @@ DECLARE @major INT;
 
 SELECT @area= 'WI003'; --Enter State Abbreviation or Soil Survey Area i.e. WI or WI025
 SELECT @area_type = LEN (@area); --determines number of characters of area 2-State, 5- Soil Survey Area
-SELECT @major = 0; -- Enter 0 for major component, enter 1 for all components
+SELECT @InRangeTop = 0;
+SELECT @InRangeBot = 15;
+
 
 SELECT TOP 1000 l.areasymbol, muname, compname, c1.cokey, ch1.chkey, hzdept_r, hzdepb_r,  CASE WHEN hzdepb_r IS NULL THEN NULL
 WHEN hzdept_r IS NULL THEN NULL
 WHEN hzdept_r > hzdepb_r THEN NULL
 WHEN hzdept_r = hzdepb_r THEN NULL ELSE
-CASE WHEN hzdepb_r > 15 THEN 15 ELSE hzdepb_r END END - hzdept_r AS thickness,
+CASE WHEN hzdepb_r > @InRangeBot THEN @InRangeBot ELSE hzdepb_r END END - hzdept_r AS thickness,
 hzname, desgnmaster, --lieutex ,
-kwfact, taxorder
+kwfact, taxorder, 
+CASE WHEN hzdept_r > @InRangeBot THEN 0
+WHEN hzdepb_r < @InRangeTop THEN 0
+WHEN hzdepb_r <= @InRangeBot THEN hzdepb_r  WHEN hzdepb_r > @InRangeBot and hzdept_r < @InRangeBot THEN @InRangeBot ELSE @InRangeTop END-CASE WHEN hzdepb_r < @InRangeTop THEN 0
+WHEN hzdept_r >@InRangeBot THEN 0 
+WHEN hzdepb_r >= @InRangeTop AND hzdept_r < @InRangeTop THEN @InRangeTop 
+WHEN hzdept_r < @InRangeTop THEN 0
+WHEN hzdept_r < @InRangeBot then hzdept_r ELSE @InRangeTop END AS thickness2
 
 
 FROM  sacatalog AS  sc
  INNER JOIN legend  AS l ON l.areasymbol = sc.areasymbol
  INNER JOIN  mapunit AS mu ON mu.lkey = l.lkey AND  CASE WHEN @area_type = 2 THEN LEFT (l.areasymbol, 2) ELSE l.areasymbol END = @area
-INNER JOIN  component AS c1 ON c1.mukey = mu.mukey AND majcompflag = 'Yes'
-INNER JOIN chorizon AS ch1 ON ch1.cokey=c1.cokey AND hzdept_r < 15 
-AND (((ch1.hzdept_r)=(SELECT Min(chorizon.hzdept_r) AS MinOfhzdept_r
+INNER JOIN  component AS c1 ON c1.mukey = mu.mukey AND majcompflag = 'Yes' AND c1.cokey = 21387231
+INNER JOIN chorizon AS ch1 ON ch1.cokey=c1.cokey AND hzdepb_r > @InRangeTop AND hzdept_r < @InRangeBot 
+AND (((CASE WHEN hzdept_r > @InRangeBot THEN 0
+WHEN hzdepb_r < @InRangeTop THEN 0
+WHEN hzdepb_r <= @InRangeBot THEN hzdepb_r  WHEN hzdepb_r > @InRangeBot and hzdept_r < @InRangeBot THEN @InRangeBot ELSE @InRangeTop END-CASE WHEN hzdepb_r < @InRangeTop THEN 0
+WHEN hzdept_r >@InRangeBot THEN 0 
+WHEN hzdepb_r >= @InRangeTop AND hzdept_r < @InRangeTop THEN @InRangeTop 
+WHEN hzdept_r < @InRangeTop THEN 0
+WHEN hzdept_r < @InRangeBot then hzdept_r ELSE @InRangeTop END  )
+
+
+=(SELECT MAX(CASE WHEN hzdept_r > @InRangeBot THEN 0
+WHEN hzdepb_r < @InRangeTop THEN 0
+WHEN hzdepb_r <= @InRangeBot THEN hzdepb_r  WHEN hzdepb_r > @InRangeBot and hzdept_r < @InRangeBot THEN @InRangeBot ELSE @InRangeTop END-CASE WHEN hzdepb_r < @InRangeTop THEN 0
+WHEN hzdept_r >@InRangeBot THEN 0 
+WHEN hzdepb_r >= @InRangeTop AND hzdept_r < @InRangeTop THEN @InRangeTop 
+WHEN hzdept_r < @InRangeTop THEN 0
+WHEN hzdept_r < @InRangeBot then hzdept_r ELSE @InRangeTop END ) AS MinOfhzdept_r
 FROM chorizon INNER JOIN chtexturegrp AS cht2 ON chorizon.chkey = cht2.chkey
 AND cht2.texture Not In ('SPM','HPM', 'MPM') AND cht2.rvindicator='Yes' AND c1.cokey = chorizon.cokey )))
 
---ORDER BY areasymbol, musym, muname, mu.mukey, comppct_r DESC, cokey,  hzdept_r, hzdepb_r
+ORDER BY areasymbol, musym, muname, mu.mukey, comppct_r DESC, cokey,  hzdept_r, hzdepb_r
 
 --INNER JOIN chtexturegrp AS chtg1 ON chtg1.chkey=ch1.chkey AND chtg1.rvindicator = 'yes'
 --INNER JOIN chtexture AS cht1 ON cht1.chtgkey=cht1.chtgkey
@@ -59,4 +85,9 @@ define erosfact  arraymax(lookup(max_thickness, layer_thickness, codename(kwfact
 
 define rv if codename(taxorder) == "histosols" then 0.02 else erosfact.
 
+
+CASE WHEN hzdepb_r IS NULL THEN 0
+WHEN hzdept_r IS NULL THEN 0 ELSE hzdepb_r-hzdept_r END AS thickness, 
+CASE  WHEN hzdept_r < 15 then hzdept_r ELSE 0 END AS AGG_InRangeTop_0_15, 
+CASE  WHEN hzdepb_r <= 15 THEN hzdepb_r WHEN hzdepb_r > 15 and hzdept_r < 15 THEN 15 ELSE 0 END AS AGG_InRangeBot_0_15
 */
